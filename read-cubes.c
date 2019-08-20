420e8511ef3a3d585921878c795196f2878e412a
afr2903 <afr102903@gmail.com>
2019-08-20 17:28:39 -0500

    readcubes

--- read-cubes.c
+++ read-cubes.c
@@ -1,12 +1,73 @@
 #pragma config(Sensor, S4,     colour,         sensorEV3_GenericI2C)
-int colors[]={0,0,2,0,3,4,4,5,5,5,0,0,0,3,0,0,0,0,0};
+int colors[]={0,0,2,0,3,4,4,5,5,5,5,0,0,3,0,0,0,0,0};
 int c;
 bool vis[6], finish=false;
 int cubes[5];
+int pos[6];
+//1E 2S 3W 4N
+//RB 2 3 0 1
+//G  1 2 3 0
+//Y  3 0 1 2
+
+int deg= 320;
+int dev;
+int gyro;
+int  Kp = 110;
+int offset_r = 34; //light value
+int Tp = 70; //speed
+int turn =0;
+int powerRight=0;
+int powerLeft=0;
+int powerRight2=0;
+int powerLeft2=0;
+int error=0;
+int light_value=0;
+
+void av(int dg, int vl){
+	int aux= dg>0? 1:-1;
+	setMotorTarget(motorB, getMotorEncoder(motorB)+dg, vl);
+	setMotorSpeed(motorC, vl*aux);
+	waitUntilMotorStop(motorB);
+	setMotorSpeed(motorC, 0);
+	delay(100);
+}
+void spin(int dg){
+	while(getGyroDegrees(S3)<dg-1||getGyroDegrees(S3)>dg+1){
+		gyro= getGyroDegrees(S3);
+		string gyr= getGyroDegrees(S3);
+		displayBigTextLine(5, gyr);
+		setMotorSpeed(motorB, -(dg-gyro));
+		setMotorSpeed(motorC, (dg-gyro));
+	}
+	setMotorSpeed(motorB, 0);
+	setMotorSpeed(motorC, 0);
+	resetGyro(S3);
+	delay(300);
+}
+void lf_r(){
+	light_value= getColorReflected(S2);
+	error= light_value - offset_r;
+	turn= Kp*error;
+	turn=turn/100;
+	powerLeft= (Tp-turn)*0.16;
+	powerRight= (Tp+turn)*0.16;
+	displayBigTextLine(9, "luz: %d",light_value);
+
+	gyro= getGyroDegrees(S3);
+	string gyr= getGyroDegrees(S3);
+	displayBigTextLine(5, gyr);
+	powerRight2= (70+(gyro*2))*0.44;
+	powerLeft2= (70-(gyro*2))*0.44;
+
+	setMotorSpeed(motorB, powerRight+powerRight2);
+	setMotorSpeed(motorC, powerLeft+powerLeft2);
+}
 
 task read_cubes(){
 	for(int j=1; j<=4; j++){
 		while(true){
+			string aux= j;
+			displayBigTextLine(1, aux);
 			c=SensorValue(colour);
 			cubes[j]= colors[c];
 			if(cubes[j]!=0 && vis[colors[c]]==0){
@@ -17,38 +78,43 @@ task read_cubes(){
 	}
 	finish=true;
 }
-
-void lf(){
-	int  Kp = 200;
-	int offset = 54; //light value
-	int Tp = 70; //speed
-	int turn =0;
-	int powerRight=0;
-	int powerLeft=0;
-	int error=0;
-	int light_value=0;
-	light_value = getColorReflected(S2);
-	error = light_value - offset;
-	turn = Kp*error;
-	turn=turn/100;
-	powerRight= Tp-turn;
-	powerLeft= Tp+20+turn;
-	displayTextLine(3, "luz: %d",light_value);
-	setMotorSpeed(motorB, powerRight);
-	setMotorSpeed(motorC, powerLeft);
+task turnoff(){
+	waitUntil(getButtonPress(buttonRight)==1);
+	stopAllTasks();
 }
 
 task main(){
 	setMotorReversed(motorC, true);
-	setSoundVolume(10);
-	setMotorSpeed(motorC, 1);
+	startTask(turnoff);
+	setSoundVolume(40);
+	//setMotorSpeed(motorD, 20);
+	resetGyro(S3);
+
+	setMotorSpeed(motorC, 50);
 	while(getColorReflected(S2)>20) setMotorSpeed(motorB, 50);
+	delay(200);
 	setMotorSpeed(motorB, 0);
-	while(getColorReflected(S2)<20) setMotorSpeed(motorC, 30);
-	while(getColorReflected(S2)>20) setMotorSpeed(motorC, 30);
 	setMotorSpeed(motorC, 0);
+	repeat(2){
+		while(getColorReflected(S2)>20) setMotorSpeed(motorB, 40);
+		delay(50);
+		while(getColorReflected(S2)<20) setMotorSpeed(motorB, 40);
+		setMotorSpeed(motorB, 0);
+		delay(50);
+
+		while(getColorReflected(S2)>20) setMotorSpeed(motorC, 40);
+		delay(50);
+		while(getColorReflected(S2)<20) setMotorSpeed(motorC, 40);
+		setMotorSpeed(motorC, 0);
+		delay(50);
+	}
+	delay(600);
+
+
 	startTask(read_cubes);
-	while(!finish) lf();
+	while(!finish&&getColorReflected(S1)>10) lf_r();
+	delay(100);
+	while(getColorReflected(S1)>10) lf_r();
 	setMotorSpeed(motorB,0);
 	setMotorSpeed(motorC,0);
 	for(int j=1; j<=4; j++){
@@ -58,17 +124,18 @@ task main(){
 		if(cubes[j]==5) playSoundFile("Black");
 		delay(500);
 	}
-	while(getColorReflected(S1)>20) lf();
-	setMotorSpeed(motorB,30);
+	for(int j=1; j<=4; j++) pos[cubes[j]]=j;
+
+	av(-50, 50);
+	setMotorSpeed(motorB,40);
 	setMotorSpeed(motorC,0);
 	delay(500);
 	while(getColorReflected(S2)>20) setMotorSpeed(motorB, 40);
-
-	while(getColorReflected(S1)>20) lf();
+	spin(-90);
 	repeat(4){
 		clearTimer(T1);
-		while(time1[T1] < 500) lf();
-		while(getColorReflected(S1)>20) lf();
+		while(time1[T1] < 500) lf_r();
+		while(getColorReflected(S1)>20) lf_r();
 	}
 	setMotorSpeed(motorB,0);
 	setMotorSpeed(motorC,0);
